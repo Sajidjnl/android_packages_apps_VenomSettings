Project-Xtended/packages_apps_Xtensions

HW Keys customization support [2/2]
Based on DUI

jhenrique09 edits: make it DUI independent

Original commit message:

Same robust action library as used on software navigation. Supports
single tap, double tap, and long press. As a precautionary measure,
single tap back and single tap home are fixed and can not be changed.
Camera button actions are not supported at this time. We will bring
in wake key support at a later time.

Also includes:

DUI: Initial checkin for Oreo [4/7]

src/com/msm/xtended/preferences/ActionPreference.java
@@ -1,0 +1,136 @@
+/*
+ * Copyright (C) 2015 TeamEos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Simple preference class implementing ActionHolder interface to assign
+ * actions to buttons. It is ABSOLUTELY IMPERITIVE that the preference
+ * key is identical to the target ConfigMap tag in ActionConstants 
+ */
+
+package com.msm.xtended.preferences;
+
+import java.util.Map;
+
+import com.android.internal.util.hwkeys.ActionConstants.ConfigMap;
+import com.android.internal.util.hwkeys.ActionConstants.Defaults;
+import com.android.internal.util.hwkeys.ActionHolder;
+import com.android.internal.util.hwkeys.Config.ActionConfig;
+import com.android.internal.util.hwkeys.Config.ButtonConfig;
+
+import android.content.Context;
+import android.support.v7.preference.Preference;
+import android.util.AttributeSet;
+
+public class ActionPreference extends Preference implements ActionHolder {
+    private Defaults mDefaults;
+    private ConfigMap mMap;
+    private ActionConfig mAction;
+    private ActionConfig mDefaultAction;
+
+    public ActionPreference(Context context) {
+        this(context, null);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs, int defStyleAttr,
+            int defStyleRes) {
+        super(context, attrs);
+    }
+
+    @Override
+    public String getTag() {
+        return this.getKey();
+    }
+
+    @Override
+    public void setTag(String tag) {
+        this.setKey(tag);
+    }
+
+    @Override
+    public Defaults getDefaults() {
+        return mDefaults;
+    }
+
+    @Override
+    public void setDefaults(Defaults defaults) {
+        mDefaults = defaults;
+        final String tag = this.getKey();
+        for (Map.Entry<String, ConfigMap> entry : defaults.getActionMap().entrySet()) {
+            if (((String) entry.getKey()).equals(tag)) {
+                mMap = entry.getValue();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public ConfigMap getConfigMap() {
+        return mMap;
+    }
+
+    @Override
+    public void setConfigMap(ConfigMap map) {
+        mMap = map;
+    }
+
+    @Override
+    public ButtonConfig getButtonConfig() {
+        return null;
+    }
+
+    @Override
+    public void setButtonConfig(ButtonConfig button) {
+    }
+
+    @Override
+    public ActionConfig getActionConfig() {
+        return mAction;
+    }
+
+    @Override
+    public void setActionConfig(ActionConfig action) {
+        mAction = action;
+        this.setSummary(action.getLabel());
+    }
+
+    @Override
+    public ButtonConfig getDefaultButtonConfig() {
+        return null;
+    }
+
+    @Override
+    public void setDefaultButtonConfig(ButtonConfig button) {
+
+    }
+
+    @Override
+    public ActionConfig getDefaultActionConfig() {
+        return mDefaultAction;
+    }
+
+    @Override
+    public void setDefaultActionConfig(ActionConfig action) {
+        mDefaultAction = action;
+    }
+}
+
+

src/com/msm/xtended/preferences/CustomActionListAdapter.java
@@ -1,0 +1,125 @@
+/*
+ * Copyright (C) 2015 TeamEos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Adapter for displaying custom actions in a list
+ */
+
+package com.msm.xtended.preferences;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.internal.util.hwkeys.ActionHandler;
+import com.android.internal.util.hwkeys.Config.ActionConfig;
+
+import com.android.settings.R;
+
+public class CustomActionListAdapter extends BaseAdapter {
+    private LayoutInflater mInflater;
+    private Context mContext;
+    private List<ActionConfig> mCustomActions = new ArrayList<ActionConfig>();
+
+    public CustomActionListAdapter(Context context) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        reloadActions();
+    }
+
+    private void reloadActions() {
+        mCustomActions.clear();
+        mCustomActions.addAll(ActionHandler.getSystemActions(mContext));
+        notifyDataSetChanged();
+    }
+
+    public void removeAction(String action) {
+        int index = -1;
+        for (int i = 0; i < mCustomActions.size(); i++) {
+            if (TextUtils.equals(mCustomActions.get(i).getAction(), action)) {
+                index = i;
+                break;
+            }
+        }
+        if (index > -1) {
+            mCustomActions.remove(index);
+            notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mCustomActions.size();
+    }
+
+    @Override
+    public ActionConfig getItem(int position) {
+        return mCustomActions.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        final Context ctx = mContext;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = mInflater.inflate(R.layout.custom_action_item, null, false);
+            holder = new ViewHolder();
+            convertView.setTag(holder);
+            holder.title = (TextView) convertView.findViewById(com.android.internal.R.id.title);
+            holder.summary = (TextView) convertView
+                    .findViewById(com.android.internal.R.id.summary);
+            holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) holder.icon.getLayoutParams();
+            params.width = holder.icon.getMaxWidth();
+            params.height = holder.icon.getMaxHeight();
+            holder.icon.setLayoutParams(params);
+            holder.icon.setScaleType(ScaleType.CENTER);
+            holder.icon.setCropToPadding(true);
+        }
+
+        ActionConfig config = getItem(position);
+        holder.title.setText(config.getLabel());
+        holder.icon.setBackgroundResource(R.drawable.fab_accent);
+        holder.icon.setImageDrawable(config.getDefaultIcon(ctx));
+        holder.summary.setVisibility(View.GONE);
+
+        return convertView;
+
+    }
+
+    private static class ViewHolder {
+        TextView title;
+        TextView summary;
+        ImageView icon;
+    }
+}
+
+

src/com/msm/xtended/fragments/ButtonSettings.java
@@ -35,24 +35,125 @@ import android.provider.Settings;
 import com.android.settings.R;
 
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.util.hwkeys.ActionConstants;
+import com.android.internal.util.hwkeys.ActionUtils;
+import com.msm.xtended.preferences.ActionFragment;
 
 import com.android.internal.logging.nano.MetricsProto;
 
-public class ButtonSettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener{
+public class ButtonSettings extends ActionFragment implements OnPreferenceChangeListener {
+
+    private static final String HWKEY_DISABLE = "hardware_keys_disable";
+
+    // category keys
+    private static final String CATEGORY_HWKEY = "hardware_keys";
+    private static final String CATEGORY_HOME = "home_key";
+    private static final String CATEGORY_MENU = "menu_key";
+    private static final String CATEGORY_BACK = "back_key";
+    private static final String CATEGORY_ASSIST = "assist_key";
+    private static final String CATEGORY_APPSWITCH = "app_switch_key";
+
+    // Masks for checking presence of hardware keys.
+    // Must match values in frameworks/base/core/res/res/values/config.xml
+    // Masks for checking presence of hardware keys.
+    // Must match values in frameworks/base/core/res/res/values/config.xml
+    public static final int KEY_MASK_HOME = 0x01;
+    public static final int KEY_MASK_BACK = 0x02;
+    public static final int KEY_MASK_MENU = 0x04;
+    public static final int KEY_MASK_ASSIST = 0x08;
+    public static final int KEY_MASK_APP_SWITCH = 0x10;
+    public static final int KEY_MASK_CAMERA = 0x20;
+    public static final int KEY_MASK_VOLUME = 0x40;
+
+    private SwitchPreference mHwKeyDisable;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.x_settings_button);
 
+        final Resources res = getResources();
+        final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        final boolean needsNavbar = ActionUtils.hasNavbarByDefault(getActivity());
+        final PreferenceCategory hwkeyCat = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_HWKEY);
+        int keysDisabled = 0;
+        if (!needsNavbar) {
+            mHwKeyDisable = (SwitchPreference) findPreference(HWKEY_DISABLE);
+            keysDisabled = Settings.Secure.getIntForUser(getContentResolver(),
+                    Settings.Secure.HARDWARE_KEYS_DISABLE, 0,
+                    UserHandle.USER_CURRENT);
+            mHwKeyDisable.setChecked(keysDisabled != 0);
+            mHwKeyDisable.setOnPreferenceChangeListener(this);
+        } else {
+            prefScreen.removePreference(hwkeyCat);
+        }
+
+        // bits for hardware keys present on device
+        final int deviceKeys = getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys);
+        // read bits for present hardware keys
+        final boolean hasHomeKey = (deviceKeys & KEY_MASK_HOME) != 0;
+        final boolean hasBackKey = (deviceKeys & KEY_MASK_BACK) != 0;
+        final boolean hasMenuKey = (deviceKeys & KEY_MASK_MENU) != 0;
+        final boolean hasAssistKey = (deviceKeys & KEY_MASK_ASSIST) != 0;
+        final boolean hasAppSwitchKey = (deviceKeys & KEY_MASK_APP_SWITCH) != 0;
+        // load categories and init/remove preferences based on device
+        // configuration
+        final PreferenceCategory backCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_BACK);
+        final PreferenceCategory homeCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_HOME);
+        final PreferenceCategory menuCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_MENU);
+        final PreferenceCategory assistCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_ASSIST);
+        final PreferenceCategory appSwitchCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_APPSWITCH);
+        // back key
+        if (!hasBackKey) {
+            prefScreen.removePreference(backCategory);
+        }
+        // home key
+        if (!hasHomeKey) {
+            prefScreen.removePreference(homeCategory);
+        }
+        // App switch key (recents)
+        if (!hasAppSwitchKey) {
+            prefScreen.removePreference(appSwitchCategory);
+        }
+        // menu key
+        if (!hasMenuKey) {
+            prefScreen.removePreference(menuCategory);
+        }
+        // search/assist key
+        if (!hasAssistKey) {
+            prefScreen.removePreference(assistCategory);
+        }
+        // let super know we can load ActionPreferences
+        onPreferenceScreenLoaded(ActionConstants.getDefaults(ActionConstants.HWKEYS));
+        // load preferences first
+        setActionPreferencesEnabled(keysDisabled == 0);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
 
+        if (preference == mHwKeyDisable) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(), Settings.Secure.HARDWARE_KEYS_DISABLE,
+                    value ? 1 : 0);
+            setActionPreferencesEnabled(!value);
+            return true;
+        }
         return false;
+    }
+
+    @Override
+    protected boolean usesExtendedActionsList() {
+        return true;
     }
 
     @Override

Sajidjnl/android_packages_apps_VenomSettings

src/com/superior/settings/preferences/ActionPreference.java
@@ -1,0 +1,136 @@
+/*
+ * Copyright (C) 2015 TeamEos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Simple preference class implementing ActionHolder interface to assign
+ * actions to buttons. It is ABSOLUTELY IMPERITIVE that the preference
+ * key is identical to the target ConfigMap tag in ActionConstants 
+ */
+
+package com.superior.settings.preferences;
+
+import java.util.Map;
+
+import com.android.internal.util.hwkeys.ActionConstants.ConfigMap;
+import com.android.internal.util.hwkeys.ActionConstants.Defaults;
+import com.android.internal.util.hwkeys.ActionHolder;
+import com.android.internal.util.hwkeys.Config.ActionConfig;
+import com.android.internal.util.hwkeys.Config.ButtonConfig;
+
+import android.content.Context;
+import android.support.v7.preference.Preference;
+import android.util.AttributeSet;
+
+public class ActionPreference extends Preference implements ActionHolder {
+    private Defaults mDefaults;
+    private ConfigMap mMap;
+    private ActionConfig mAction;
+    private ActionConfig mDefaultAction;
+
+    public ActionPreference(Context context) {
+        this(context, null);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs, int defStyleAttr,
+            int defStyleRes) {
+        super(context, attrs);
+    }
+
+    @Override
+    public String getTag() {
+        return this.getKey();
+    }
+
+    @Override
+    public void setTag(String tag) {
+        this.setKey(tag);
+    }
+
+    @Override
+    public Defaults getDefaults() {
+        return mDefaults;
+    }
+
+    @Override
+    public void setDefaults(Defaults defaults) {
+        mDefaults = defaults;
+        final String tag = this.getKey();
+        for (Map.Entry<String, ConfigMap> entry : defaults.getActionMap().entrySet()) {
+            if (((String) entry.getKey()).equals(tag)) {
+                mMap = entry.getValue();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public ConfigMap getConfigMap() {
+        return mMap;
+    }
+
+    @Override
+    public void setConfigMap(ConfigMap map) {
+        mMap = map;
+    }
+
+    @Override
+    public ButtonConfig getButtonConfig() {
+        return null;
+    }
+
+    @Override
+    public void setButtonConfig(ButtonConfig button) {
+    }
+
+    @Override
+    public ActionConfig getActionConfig() {
+        return mAction;
+    }
+
+    @Override
+    public void setActionConfig(ActionConfig action) {
+        mAction = action;
+        this.setSummary(action.getLabel());
+    }
+
+    @Override
+    public ButtonConfig getDefaultButtonConfig() {
+        return null;
+    }
+
+    @Override
+    public void setDefaultButtonConfig(ButtonConfig button) {
+
+    }
+
+    @Override
+    public ActionConfig getDefaultActionConfig() {
+        return mDefaultAction;
+    }
+
+    @Override
+    public void setDefaultActionConfig(ActionConfig action) {
+        mDefaultAction = action;
+    }
+}
+
+

src/com/superior/settings/preferences/CustomActionListAdapter.java
@@ -1,0 +1,125 @@
+/*
+ * Copyright (C) 2015 TeamEos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Adapter for displaying custom actions in a list
+ */
+
+package com.superior.settings.preferences;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.internal.util.hwkeys.ActionHandler;
+import com.android.internal.util.hwkeys.Config.ActionConfig;
+
+import com.android.settings.R;
+
+public class CustomActionListAdapter extends BaseAdapter {
+    private LayoutInflater mInflater;
+    private Context mContext;
+    private List<ActionConfig> mCustomActions = new ArrayList<ActionConfig>();
+
+    public CustomActionListAdapter(Context context) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        reloadActions();
+    }
+
+    private void reloadActions() {
+        mCustomActions.clear();
+        mCustomActions.addAll(ActionHandler.getSystemActions(mContext));
+        notifyDataSetChanged();
+    }
+
+    public void removeAction(String action) {
+        int index = -1;
+        for (int i = 0; i < mCustomActions.size(); i++) {
+            if (TextUtils.equals(mCustomActions.get(i).getAction(), action)) {
+                index = i;
+                break;
+            }
+        }
+        if (index > -1) {
+            mCustomActions.remove(index);
+            notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mCustomActions.size();
+    }
+
+    @Override
+    public ActionConfig getItem(int position) {
+        return mCustomActions.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        final Context ctx = mContext;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = mInflater.inflate(R.layout.custom_action_item, null, false);
+            holder = new ViewHolder();
+            convertView.setTag(holder);
+            holder.title = (TextView) convertView.findViewById(com.android.internal.R.id.title);
+            holder.summary = (TextView) convertView
+                    .findViewById(com.android.internal.R.id.summary);
+            holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) holder.icon.getLayoutParams();
+            params.width = holder.icon.getMaxWidth();
+            params.height = holder.icon.getMaxHeight();
+            holder.icon.setLayoutParams(params);
+            holder.icon.setScaleType(ScaleType.CENTER);
+            holder.icon.setCropToPadding(true);
+        }
+
+        ActionConfig config = getItem(position);
+        holder.title.setText(config.getLabel());
+        holder.icon.setBackgroundResource(R.drawable.fab_accent);
+        holder.icon.setImageDrawable(config.getDefaultIcon(ctx));
+        holder.summary.setVisibility(View.GONE);
+
+        return convertView;
+
+    }
+
+    private static class ViewHolder {
+        TextView title;
+        TextView summary;
+        ImageView icon;
+    }
+}
+
+

src/com/venom/settings/fragments/ButtonSettings.java
@@ -32,19 +32,41 @@ import android.provider.Settings;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.venom.settings.preferences.CustomSeekBarPreference;
+import com.android.internal.util.hwkeys.ActionConstants;
+import com.android.internal.util.hwkeys.ActionUtils;
+import com.venom.settings.preferences.ActionFragment;
+import com.venom.settings.preferences.CustomSeekBarPreference;
+
 import com.android.internal.logging.nano.MetricsProto;
 import com.venom.settings.R; 
 
-public class ButtonSettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener{
+public class ButtonSettings extends ActionFragment implements OnPreferenceChangeListener {
+
+    private static final String HWKEY_DISABLE = "hardware_keys_disable";
+     // category keys
+    private static final String CATEGORY_HWKEY = "hardware_keys";
+    private static final String CATEGORY_HOME = "home_key";
+    private static final String CATEGORY_MENU = "menu_key";
+    private static final String CATEGORY_BACK = "back_key";
+    private static final String CATEGORY_ASSIST = "assist_key";
+    private static final String CATEGORY_APPSWITCH = "app_switch_key";
+     // Masks for checking presence of hardware keys.
+    // Must match values in frameworks/base/core/res/res/values/config.xml
+    // Masks for checking presence of hardware keys.
+    // Must match values in frameworks/base/core/res/res/values/config.xml
+    public static final int KEY_MASK_HOME = 0x01;
+    public static final int KEY_MASK_BACK = 0x02;
+    public static final int KEY_MASK_MENU = 0x04;
+    public static final int KEY_MASK_ASSIST = 0x08;
+    public static final int KEY_MASK_APP_SWITCH = 0x10;
+    public static final int KEY_MASK_CAMERA = 0x20;
+    public static final int KEY_MASK_VOLUME = 0x40;
+     private SwitchPreference mHwKeyDisable;
 
     //Keys
     private static final String KEY_BUTTON_BRIGHTNESS = "button_brightness";
     private static final String KEY_BUTTON_BRIGHTNESS_SW = "button_brightness_sw";
     private static final String KEY_BACKLIGHT_TIMEOUT = "backlight_timeout";
-
-    // category keys
-    private static final String CATEGORY_HWKEY = "hardware_keys";
 
     private ListPreference mBacklightTimeout;
     private CustomSeekBarPreference mButtonBrightness;
@@ -55,7 +77,69 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.venom_settings_button);
 
+        final Resources res = getResources();
+        final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        final boolean needsNavbar = ActionUtils.hasNavbarByDefault(getActivity());
+        final PreferenceCategory hwkeyCat = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_HWKEY);
+        int keysDisabled = 0;
+        if (!needsNavbar) {
+            mHwKeyDisable = (SwitchPreference) findPreference(HWKEY_DISABLE);
+            keysDisabled = Settings.Secure.getIntForUser(getContentResolver(),
+                    Settings.Secure.HARDWARE_KEYS_DISABLE, 0,
+                    UserHandle.USER_CURRENT);
+            mHwKeyDisable.setChecked(keysDisabled != 0);
+            mHwKeyDisable.setOnPreferenceChangeListener(this);
+        } else {
+            prefScreen.removePreference(hwkeyCat);
+        }
+         // bits for hardware keys present on device
+        final int deviceKeys = getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys);
+        // read bits for present hardware keys
+        final boolean hasHomeKey = (deviceKeys & KEY_MASK_HOME) != 0;
+        final boolean hasBackKey = (deviceKeys & KEY_MASK_BACK) != 0;
+        final boolean hasMenuKey = (deviceKeys & KEY_MASK_MENU) != 0;
+        final boolean hasAssistKey = (deviceKeys & KEY_MASK_ASSIST) != 0;
+        final boolean hasAppSwitchKey = (deviceKeys & KEY_MASK_APP_SWITCH) != 0;
+        // load categories and init/remove preferences based on device
+        // configuration
+        final PreferenceCategory backCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_BACK);
+        final PreferenceCategory homeCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_HOME);
+        final PreferenceCategory menuCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_MENU);
+        final PreferenceCategory assistCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_ASSIST);
+        final PreferenceCategory appSwitchCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_APPSWITCH);
+        // back key
+        if (!hasBackKey) {
+            prefScreen.removePreference(backCategory);
+        }
+        // home key
+        if (!hasHomeKey) {
+            prefScreen.removePreference(homeCategory);
+        }
+        // App switch key (recents)
+        if (!hasAppSwitchKey) {
+            prefScreen.removePreference(appSwitchCategory);
+        }
+        // menu key
+        if (!hasMenuKey) {
+            prefScreen.removePreference(menuCategory);
+        }
+        // search/assist key
+        if (!hasAssistKey) {
+            prefScreen.removePreference(assistCategory);
+        }
+        // let super know we can load ActionPreferences
+        onPreferenceScreenLoaded(ActionConstants.getDefaults(ActionConstants.HWKEYS));
+        // load preferences first
+        setActionPreferencesEnabled(keysDisabled == 0);
 
         final boolean variableBrightness = getResources().getBoolean(
                 com.android.internal.R.bool.config_deviceHasVariableButtonBrightness);
@@ -115,8 +199,19 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.BUTTON_BRIGHTNESS, value ? 1 : 0);
             return true;
+        } else if (preference == mHwKeyDisable) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(), Settings.Secure.HARDWARE_KEYS_DISABLE,
+                    value ? 1 : 0);
+            setActionPreferencesEnabled(!value);
+            return true;
         }
         return false;
+    }
+
+    @Override
+    protected boolean usesExtendedActionsList() {
+        return true;
     }
 
     @Override